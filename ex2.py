d405c3153cbce26feb302c44534f09ce757a6060
APF2000 <arthurpfonseca@gmail.com>
2020-03-31 18:39:13 -0300

    Integral batata + super matriz

--- ex2.py
+++ ex2.py
@@ -13,10 +13,30 @@ def integral(u1, u2, s0, sN, deltaT):
 
     return sum * deltaT / 2
 
+def linearSystem(K, funcs, dr, deltaT):
+    B = m.criarMatriz(K, K, False)
+    c = m.criarMatriz(K, 1, False)
+
+    for i in range(K):
+        for j in range(i):
+            B[i][j] = integral(funcs[i], funcs[j], 0, 1, deltaT)
+            B[j][i] = B[i][j]
+        B[i][i] = integral(funcs[i], funcs[i], 0, 1, deltaT)
+
+    for i in range(K):
+        c[i][0] = integral(funcs[i], dr, 0, 1, deltaT)
+
+    return B, c
+
 def y1(x):
-    return 1
+    return x
 
 def y2(x):
-    return math.exp(x)
+    return x ** 2
 
-print(integral(y1, y2, math.log(1.2345), math.log(2), 0.0000001))
+def dr(x):
+    return 1
+
+B, c = linearSystem(2, [y1, y2], dr, 0.001)
+print(B)
+print(c)
