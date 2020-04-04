06bb6a99239ec88e31cfe66383d8d73c4d6c08de
APF2000 <arthurpfonseca@gmail.com>
2020-04-04 18:14:17 -0300

    Sla

--- ex2.py
+++ ex2.py
@@ -1,5 +1,7 @@
 import Matrizes as m
 import math
+import numpy as np
+import ex1
 
 def integral(u1, u2, s0, sN, deltaT):
     n = int( (sN - s0) / deltaT )
@@ -28,15 +30,46 @@ def linearSystem(K, funcs, dr, deltaT):
 
     return B, c
 
-def y1(x):
+def readNPY(name):
+    return np.load(name)
+
+
+dr = readNPY("dr3.npy")
+def funDr(s):
+    nt = 1000
+    pos = int(s * nt)
+
+    return dr[pos]
+
+def createFuncs():
+    return 0
+
+nt = 1000
+deltaT = 1 / nt
+deltaX = 0.01
+c2 = 20
+T = 1
+
+def ukx(xr):
+    j = int( xr / deltaX )
+
+    x = []
+
+    matrix = m.criarMatriz(1000, 1000, "a")
+    firstTime = True
+    for i in range(nt + 1):
+        aux = ex1.EDO(matrix=matrix, nt=nt, deltaX=deltaX, c2=c2, T=T, i=i, j=j, firstTime=firstTime)
+        x.append(aux[0])
+        matrix = aux[1]
+        firstTime = False
+
     return x
 
-def y2(x):
-    return x ** 2
+print(ukx(0.7))
 
-def dr(x):
-    return 1
 
-B, c = linearSystem(2, [y1, y2], dr, 0.001)
-print(B)
-print(c)
+
+def ukt(t):
+    nt = 1000
+    pos = int(t * nt)
+    return uk(x)[pos]
